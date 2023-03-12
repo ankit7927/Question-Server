6680a18066ef3344901671536a266e02814b8078
ankit prajapati <code7lancer@gmail.com>
2023-03-12 14:04:04 -0700

    updated cors middlewere and env

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+require('dotenv').config()
 var express = require("express");
 var logger = require("morgan");
 var cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 require('dotenv').config()
 require("./database/conn");
 
 app = express();
 const port = process.env.PORT || 4000
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(logger("dev"));
-
 app.use(express.json());
 app.use(
   express.urlencoded({
