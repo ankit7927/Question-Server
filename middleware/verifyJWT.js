f4f7a04064ccd6d25e60d442c74bbca3b3949b4a
ankit prajapati <code7lancer@gmail.com>
2023-03-13 13:06:37 -0700

    removed some log statments

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,12 +2,12 @@ const jwt = require("jsonwebtoken")
 
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization || "";
-    console.log(authHeader);
+    
     if (!authHeader?.startsWith("Bearer "))
         return res.status(401).json({ message: "Unauthorized" });
 
     const token = authHeader.split(" ")[1]
-    console.log(token);
+    
     jwt.verify(token, "my-access-token-secret",
         (err, decoded) => {
             if (err) return res.status(403).json({ message: "forbidden" })
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,14 +1,13 @@
 var express = require("express");
-const { verifyToken } = require("../extras/JWTHelper");
 const userController = require("../controllers/userController");
 const verifyJWT = require("../middleware/verifyJWT");
 var router = express.Router();
 
 router
     .post("/signup", userController.signup)
-    .patch("/profile", verifyToken, userController.updateProfile)
+    .patch("/profile", verifyJWT, userController.updateProfile)
     .get("/questions",verifyJWT, userController.question)
     // save or remove question from acc
-    .get("/save-rem/:queID", verifyToken, userController.saveRemoveQues)
+    .get("/save-rem/:queID", verifyJWT, userController.saveRemoveQues)
 
 module.exports = router;
