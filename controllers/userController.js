fb4522d009e87b19c84c0bbec71335462957e172
ankit prajapati <code7lancer@gmail.com>
2023-03-12 17:51:23 -0700

    made more efficient user point

---
+++ controllers/userController.js
@@ -0,0 +1,104 @@
+const asyncHandler = require("express-async-handler")
+const userSchema = require("../database/schemas/userSchema");
+const questionSchema = require("../database/schemas/questionSchema");
+const { generateToken } = require("../extras/JWTHelper");
+const bcrypt = require('bcrypt');
+
+const userController = {}
+
+userController.signup = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: 'all fields required' });
+    } else if (password.length < 8) {
+        return res.status(400).json({ message: 'password length is too short' });
+    }
+
+    const existingUser = await userSchema.findOne({ email: email }).lean().exec()
+    if (existingUser) {
+        return res.status(409).json({ message: 'Email alredy used' });
+    }
+
+    const hashpass = await bcrypt.hash(password, 10)
+
+    const newuser = { name, email, "password": hashpass }
+
+    const user = await userSchema.create(newuser)
+
+    if (user) {
+        res.status(201).json({ message: "user created" })
+    } else {
+        res.status(400).json({ message: "invalid user data received" })
+    }
+})
+
+userController.signin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'all fields required' });
+    }
+
+    const existingUser = await userSchema.findOne({ email: email }).lean().exec()
+
+    if (!existingUser) {
+        return res.status(400).json({ message: 'email not found' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, existingUser.password);
+    if (!passwordMatch) {
+        return res.status(404).send('wrong password..');
+    }
+    res.status(200).json({
+        "token": generateToken(existingUser._id),
+        "name": existingUser.name,
+        "email": existingUser.email,
+        "userID": existingUser._id
+    })
+})
+
+userController.updateProfile = asyncHandler(async (req, res) => {
+    const userID = req.user._id;
+    const { name, email } = req.body
+
+    if (!email || !name) {
+        return res.status(400).json({ message: 'all fields required' });
+    }
+
+    const existingUser = await userSchema.findById({ _id: userID })
+        .select("name email").select("-password").exec()
+
+    if (!existingUser) {
+        return res.status(400).json({ message: 'user not found' });
+    }
+
+    existingUser.name = name;
+    existingUser.email = email;
+
+    const updatedUser = await existingUser.save()
+
+    res.json(updatedUser)
+})
+
+userController.question = asyncHandler(async (req, res) => {
+    const userQuestion = await userSchema.findOne({ _id: req.user._id })
+        .select("question")
+        .lean().exec()
+
+    let quesObj = {}
+
+    quesObj.asked = await questionSchema.find({ _id: { $in: userQuestion.question.asked } })
+        .select("question createdAt votes")
+
+    quesObj.answerd = await questionSchema.find({ _id: { $in: userQuestion.question.answerd } })
+        .select("question createdAt votes")
+
+    quesObj.saved = await questionSchema.find({ _id: { $in: userQuestion.question.saved } })
+        .select("question createdAt votes")
+
+    res.json(quesObj)
+})
+
+
+module.exports = userController;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,91 +1,12 @@
 var express = require("express");
-const userSchema = require("../database/schemas/userSchema");
-const questionSchema = require("../database/schemas/questionSchema");
-const { generateToken } = require("../extras/JWTHelper");
 const { verifyToken } = require("../extras/JWTHelper");
-const bcrypt = require('bcrypt');
+const userController = require("../controllers/userController");
 var router = express.Router();
 
-// user signup
-router.post("/signup", async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-
-        const existingUser = await userSchema.findOne({ email: email })
-        if (existingUser) {
-            return res.status(500).send('Email alredy used...');
-        }
-
-        const newUser = new userSchema({
-            name: name,
-            email: email,
-            password: await bcrypt.hash(password, 10),
-        });
-
-        newUser.save((err) => {
-            if (err) {
-                throw err
-            } else {
-                return res.status(201).send("registerd")
-            }
-        });
-    } catch (error) {
-        return res.status(500).send(error)
-    }
-});
-
-// user login
-router.post("/signin", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        const existingUser = await userSchema.findOne({ email: email })
-        if (!existingUser) {
-            return res.status(404).send('Wrong Email..');
-        }
-
-        const passwordMatch = await bcrypt.compare(password, existingUser.password);
-        if (!passwordMatch) {
-            return res.status(404).send('wrong password..');
-        }
-
-        res.status(200).json({
-            "token": generateToken(existingUser._id),
-            "name": existingUser.name,
-            "email": existingUser.email,
-            "userID": existingUser._id
-        })
-    } catch (error) {
-        return res.status(500).send(error)
-    }
-});
-
-router.put("/profile", verifyToken, (req, res) => {
-    const userID = req.user._id;
-    const { name, email } = req.body
-    userSchema.findByIdAndUpdate({ _id: userID },
-        {
-            $set: {
-                name: name,
-                email: email
-            }
-        }).then(data => {
-            return res.send(data);
-        }).catch(err => { return res.status(500).send(err); })
-})
-
-router.get("/questions", verifyToken, (req, res) => {
-    userSchema.findOne({ _id: req.user._id }, { question: 1 })
-        .then(async data => {
-            let quesObj = {}
-            quesObj.asked = await questionSchema.find({ _id: { $in: data.question.asked } })
-                .select("question createdAt votes")
-            quesObj.answerd = await questionSchema.find({ _id: { $in: data.question.answerd } })
-                .select("question createdAt votes")
-            quesObj.saved = await questionSchema.find({ _id: { $in: data.question.saved } })
-                .select("question createdAt votes")
-            return res.send(quesObj)
-        }).catch(err => { return res.status(500).send(err); })
-})
+router
+    .post("/signup", userController.signup)
+    .post("/signin", userController.signin)
+    .patch("/profile", verifyToken, userController.updateProfile)
+    .get("/question", verifyToken, userController.question)
 
 module.exports = router;
