e1e3225f1058f77d95b1128221a65ae56d8c8930
ankit prajapati <code7lancer@gmail.com>
2023-03-13 10:27:35 -0700

    applied middleware

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,19 +1,19 @@
 var express = require("express");
 const questionController = require("../controllers/questionController");
-const { verifyToken } = require("../extras/JWTHelper");
+const verifyJWT = require("../middleware/verifyJWT");
 var router = express.Router();
 
 router
     .get("/", questionController.questionQuery)
     .get("/index", questionController.indexQuestions)
-    .post("/new-question", verifyToken, questionController.newQuestion)
+    .post("/new-question", verifyJWT, questionController.newQuestion)
     .post("/answer", questionController.newAnswer)
     .get("/latest", questionController.latestQuestion)
     .get("/unanswred", questionController.unanswred)
     .get("/que/:quesID", questionController.questionByID)
 
     //react
-    .get("/vote/:queID", verifyToken, questionController.voteQuestion)
-    .get("/vote/:queID/:ansID", verifyToken, questionController.voteAnswer)
+    .get("/vote/:queID", verifyJWT, questionController.voteQuestion)
+    .get("/vote/:queID/:ansID", verifyJWT, questionController.voteAnswer)
 
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,13 @@
 var express = require("express");
 const { verifyToken } = require("../extras/JWTHelper");
 const userController = require("../controllers/userController");
+const verifyJWT = require("../middleware/verifyJWT");
 var router = express.Router();
 
 router
     .post("/signup", userController.signup)
-    .post("/signin", userController.signin)
     .patch("/profile", verifyToken, userController.updateProfile)
-    .get("/questions", verifyToken, userController.question)
+    .get("/questions",verifyJWT, userController.question)
     // save or remove question from acc
     .get("/save-rem/:queID", verifyToken, userController.saveRemoveQues)
 
