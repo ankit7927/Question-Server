eecbecd26b175ad285f2f73182694c46c7aeca70
ankit prajapati <code7lancer@gmail.com>
2023-03-13 03:43:41 -0700

    updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     "http://localhost:5173",
-    "http://localhost:3000",
-    "https://question-api-u69e.onrender.com"
+    "http://127.0.0.1:5173",
+    "https://ques.netlify.app"
 ]
 
 
